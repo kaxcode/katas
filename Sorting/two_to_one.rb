9370f5a29b8fa33039c2e9919742429bc1caf4c1
Kenia Valladarez <keniavalladarez@gmail.com>
2017-01-28 21:14:32 -0500

    Solved kata

---
+++ Sorting/two_to_one.rb
@@ -0,0 +1,20 @@
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, - each taken only once - coming from s1 or s2.
+#
+# Examples:
+#
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+#
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+def longest(a, b)
+  (a + b).split("").uniq.sort.join("")
+end
+
+#OR
+
+def longest(a, b)
+  (a+b).chars.uniq.sort.join
+end
