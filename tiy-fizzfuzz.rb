69021b82ee825215b05c3e472b2760b65851e1e1
Kenia Valladarez <keniavalladarez@gmail.com>
2017-02-22 11:03:14 -0500

    Update to file organization

--- tiy-fizzfuzz.rb
+++ tiy-fizzfuzz.rb
@@ -6,19 +6,22 @@
 # If a letter is a lower case vowel, replace that character with "Yard".
 
 string = "Hello WORLD!"
-string = string.chars
-big_vowel = [ "A", "E", "I", "O", "U"]
-lil_vowel = ["a", "e", "i", "o", "u"]
+
 def tiy_fizz_buzz(string)
-  string.each do |n|
-    if n == n.upcase
-      p  "Iron"
-    if n == big_vowel
-      p  "Iron Yard"
-    if n == lil_vowel
-      p  "Yard"
+  chars = string.split('')
+  chars.each { |c| c }
+  # string.each_char do |letter|
+    big_vowel = %w(A E I O U)
+    lil_vowel = %w(a e i o u)
+    if ('A'..'Z').include?(letter.to_s)
+      "Iron"
+      print letter
+    elsif big_vowel.include?(letter)
+      "IronYard"
+    elsif lil_vowel.include?(letter)
+      "Yard"
     else
-      p n
+      letter
     end
   end
 end
