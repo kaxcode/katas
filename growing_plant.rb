a06fe998c27e93e68d44ae5ebe76060cba67b958
Kenia Valladarez <keniavalladarez@gmail.com>
2017-02-27 09:33:16 -0500

    In progress...

--- growing_plant.rb
+++ growing_plant.rb
@@ -1,5 +1,9 @@
 # Each day a plant is growing by upSpeed meters. Each night that plant's height decreases by downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall. We plant the seed at the beginning of a day. We want to know when the height of the plant will reach a certain level.
 
 def growing_plant(upSpeed, downSpeed, desiredHeight)
-  #your code here
+  if ((upSpeed - downSpeed) > desiredHeight)
+    1
+  else
+    (desiredHeight / (upSpeed-downSpeed))
+  end
 end
