4747ec0b90fd2d7c312440e60e905032926b5a45
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-14 08:02:07 -0400

    Completed the kata but the tests are not passing

---
+++ count-of-positives-negatives.rb
@@ -0,0 +1,15 @@
+# Given an array of integers.
+#
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+#
+# If the input array is empty or null, return an empty array:
+
+def count_positives_sum_negatives(array)
+  if array.empty?
+    0
+  else
+    negative = array.select{ |x| x <= 0}.inject(0) { | x, y | x + y }
+    positive = array.select{ |x| x >= 0}.inject(0) { | x, y | x + y }
+    [positive, negative]
+  end
+end
